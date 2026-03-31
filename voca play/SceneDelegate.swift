9bd7fabbc39e9f3bee974107df80e685687a503a
Roman Belousov <roman.belousov.dev@gmail.com>
2026-03-31 14:30:31 +0200

    Added compilation error

--- voca play/SceneDelegate.swift
+++ voca play/SceneDelegate.swift
@@ -9,6 +9,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let appViewModel = AppViewModel()
         let contentView = ContentView(viewModel: appViewModel)
+
+        test
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = UIHostingController(rootView: contentView)
