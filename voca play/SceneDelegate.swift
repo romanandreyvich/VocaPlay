9ea81191c7ed1d4c825e0ff83d0ea5a647d2e195
Roman Belousov <roman.belousov.dev@gmail.com>
2026-03-31 13:25:31 +0200

    Added code to test slack error message

--- voca play/SceneDelegate.swift
+++ voca play/SceneDelegate.swift
@@ -10,6 +10,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let appViewModel = AppViewModel()
         let contentView = ContentView(viewModel: appViewModel)
 
+        test
+        
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = UIHostingController(rootView: contentView)
         self.window = window
